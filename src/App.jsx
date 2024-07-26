3aab7f366335050c78bfd2718251603c69c4f8a7
SAID FATAHILLAH <817931679@intranet.pajak.go.id>
2024-07-26 14:33:51 +0700

    virtualR

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import Workflow from './components/Workflow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <div className='max-w-7xl mx-auto pt-20 px-6'>
+         <HeroSection/>
+         <FeatureSection/>
+         <Workflow/>
+         <Pricing/>
+         <Testimonials/>
+         <Footer/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
